24f9ee18388f1f5f7498def248daebd85ddc2051
Mahdi Falamarzi <mahdifal@users.noreply.github.com>
2024-12-12 22:38:53 +0330

    Lesson 1 (#2)
    * INIT
    
    * [REMOVE] unnecessary files and code
    [ADD] vazirmatn font
    [ADD] `hello, friend!` example
    
    * [ADD] dynamic route example
    
    ---------
    
    Co-authored-by: Mahdi Falamarzi <mehdi.falamarzi@apsaaz.com>

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Vazirmatn } from "next/font/google";
+import "./globals.css";
+
+const vazirmatn = Vazirmatn({
+  subsets: ["latin", "arabic"],
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "پلتفرم نوبت دهی آنلاین",
+  description: "پلتفرم جستجو خدمات پزشکی رزرو و نوبت دهی آنلاین",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fa" dir="rtl" className={vazirmatn.className}>
+      <body>{children}</body>
+    </html>
+  );
+}
