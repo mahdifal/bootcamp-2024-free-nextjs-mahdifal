01bc885e73ec57dfc6d26e863afb5585be7e7977
Mahdi Falamarzi <mehdi.falamarzi@apsaaz.com>
2024-12-26 07:47:03 +0330

    [Add] error page

---
+++ src/app/error.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import Image from "next/image";
+
+import errorImage from "@/assets/vectors/error.svg";
+
+import styles from "./error.module.css";
+
+type Props = {
+  error: Error & { digest?: string };
+  reset: () => void;
+};
+
+export default function Error({ error, reset }: Props) {
+  return (
+    <div className={styles["error"]}>
+      <div className={styles.writings}>
+        <div className={styles["status-code"]}>خطا!</div>
+        <h1>یک خطای غیرمنتظره رخ داده است.</h1>
+        <p>با عرض پوزش، لطفاً با تیم پشتیبانی تماس بگیرید.</p>
+      </div>
+      <div className={styles.visuals}>
+        <Image src={errorImage} alt="" />
+      </div>
+      <div className={styles.actions}>
+        <button onClick={reset}>تلاش مجدد</button>
+      </div>
+      <div className={styles.trace}>
+        <details>
+          <summary>لاگ خطا</summary>
+          <pre dir="ltr">{error.stack}</pre>
+        </details>
+      </div>
+    </div>
+  );
+}
